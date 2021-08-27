9123c2c542d2a3ca923816a0de1a6adce2c0fa65
DellLaptop <venkat.muthu@gmail.com>
2021-08-28 00:17:00 +0100

    default ILogger with Azure App Insights

---
+++ BackendService/BackendService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogTrace("Trace - Begin - Get()");
+            _logger.LogDebug("Trace - Debug - Get()");
+            _logger.LogInformation("Trace - Information - Get()");
+            _logger.LogWarning("Trace - Warning - Get()");
+            _logger.LogError("Trace - Error - Get()");
+            _logger.LogCritical("Trace - Critical - Get()");
+
+            //throw new Exception("Test exception");
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
